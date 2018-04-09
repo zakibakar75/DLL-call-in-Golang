38b009765d3f7742593af5af56b603dca478aecb
Oren Online <zaki.bm@gmail.com>
2018-04-09 16:17:28 +0800

    Added some comments

--- zaki.go
+++ zaki.go
@@ -1,3 +1,4 @@
+/* Zaki - 6 April 2018 - Initial creation to demonstrate DLL call in Golang */
 package main
 
 import (
@@ -7,16 +8,16 @@ import (
 )
 
 func main() {
-    h, e := syscall.LoadLibrary("square.dll")
+    h, e := syscall.LoadLibrary("square.dll")   //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
     if e != nil {
         log.Fatal(e)
     }
     defer syscall.FreeLibrary(h)
-    proc, e := syscall.GetProcAddress(h, "dll_math_example")
+    proc, e := syscall.GetProcAddress(h, "dll_math_example") //One of the functions in the DLL
     if e != nil {
         log.Fatal(e)
     }
     
-    n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)
+    n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)  //Pay attention to the positioning of the parameter
     fmt.Printf("Hello dll function returns %d\n", n)
 }
